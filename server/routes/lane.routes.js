4b2bdc7e053bb1522e4afda14f3bdaf4fa345684
Kat Lasota <lasota.kat@gmail.com>
2018-10-19 13:46:22 +0200

    Add endpoints 23.4

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,16 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane  localhost:8000/api/lanes
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+// Edit a lane name
+router.route('/lanes/:laneId').put(LaneController.editLane);
 
 export default router;
--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -3,5 +3,16 @@ import * as NoteController from '../controllers/note.controller';
 
 const router = new Router();
 
+// Add a new Note
+router.route('/notes').post(NoteController.addNote);
+
+// Delete a Note by noteId
+router.route('/notes/:noteId').delete(NoteController.deleteNote);
+
+// Get all Notes
+router.route('/notes').get(NoteController.getNotes);
+
+// Edit a Note by noteId
+router.route('/notes/:noteId').put(NoteController.editNote);
 
 export default router;
